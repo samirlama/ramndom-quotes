d8d783c1c7f7959f70f37a0d358583ec7600c3a5
Sameer Lama <samirlama@bajratechnologies.com>
2022-08-21 14:03:31 +0545

    feat: add quote controller to render index and quote action

---
+++ app/services/csv_parser.rb
@@ -0,0 +1,25 @@
+require 'csv'
+
+class CsvParser
+  attr_reader :csv_file
+
+  def initialize(file)
+    @csv_file = file
+  end
+
+  def self.call(file)
+    new(file).process
+  end
+
+  def process
+    headers_converters = lambda { |h| h.downcase }
+    hash_data = Array.new
+    CSV.parse(csv_file, headers: true, header_converters: headers_converters) do |row|
+      title = row['title']
+      author = row['author']
+      hash_data << { title: title, author: author }
+    end
+
+    hash_data
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "quotes#index"
+
+  resources :quotes, only: [], path: '' do
+    collection { get 'quote', to: "quotes#quote"}
+  end
 end
